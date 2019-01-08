a1bfe7670736d23151609d9f9c2604434b44d6f2
erasmo <erasmomontes07@gmail.com>
2019-01-08 09:08:43 -0400

    Change the way to instance api client.

--- partenon/helpdesk/base.py
+++ partenon/helpdesk/base.py
@@ -18,17 +18,19 @@ class BaseManageEntity(BaseHelpDesk):
     def __init__(self, url, entity, key_name='name'):
         self._url = url
         self._entity = entity
-        self._client = faveo.APIClient()
+        self._client = faveo.APIClient
         self._key_name = key_name
 
     def get_entitys(self):
-        response = self._client.get(self._url)
+        client = self._client()
+        response = client.get(self._url)
         entity = [self._entity(**response)
                   for response in response.get('result')]
         return entity
 
     def get_by_name(self, name):
-        response = self._client.get(self._url, {'name': name})
+        client = self._client()
+        response = client.get(self._url, {'name': name})
         for item in response.get('result'):
             if item[self._key_name] == name:
                 return self._entity(**item)
